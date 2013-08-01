f860f9f0b9d3a8830b18f1f5b4ea36d871c3c98f
Rika Yoshida <nazuki@genshin.org>
2013-08-01 11:12:29 +0900

    Frameの作成テスト。Gtk2の仕様のままやってるので色々警告がでます。

--- editor_window.rb
+++ editor_window.rb
@@ -18,7 +18,33 @@ class EditorWindow
 
     @frame_list = FrameList.new
 
+    # scrolled_windowを取得して一時的にBox作成
+    # 最初からlayout.gradeにBoxを入れておくと手間が少しだけ減ります。
+    @frame_list_scrolled_window = builder["frame_list_scrolled_window"]
+    @frame_hbox = Gtk::HBox.new
+    @frame_list_scrolled_window.add_with_viewport(@frame_hbox)
+
     @add_frame_button = builder["add_frame_button"]
+    @add_frame_button.signal_connect('clicked') {
+      dialog = Gtk::FileChooserDialog.new("Open File",
+                                          $window_base,
+                                          Gtk::FileChooser::ACTION_OPEN,
+                                          nil,
+                                          [Gtk::Stock::CANCEL, Gtk::Dialog::RESPONSE_CANCEL],
+                                          [Gtk::Stock::OPEN, Gtk::Dialog::RESPONSE_ACCEPT])
+      filter = Gtk::FileFilter.new
+      filter.name = "Img File"
+      filter.add_pattern("*.png")
+      dialog.add_filter(filter)
+      if dialog.run == Gtk::Dialog::RESPONSE_ACCEPT
+        # Get file
+        frame = create_frame(dialog.filename)
+
+        @frame_hbox.pack_start(frame, false, false, 10)
+        $window_base.show_all
+      end
+      dialog.destroy
+}
 #    @add_frame_button.signal_connect('file-set') {
 #      frame = Frame.new(@add_frame_button.filename)
 #      
@@ -34,5 +60,19 @@ class EditorWindow
 
     $window_base.show_all
     Gtk.main
+  end
+
+  # とりあえずやっつけでframe作成。
+  # gladeで作っておけるかどうかは現在は不明です。
+  def create_frame(filename)
+    frame = Gtk::Frame.new
+    img = Gtk::Image.new(filename)
+    box = Gtk::VBox.new
+    box.pack_start(img, false, false, 10)
+    adjustment = Gtk::Adjustment.new(10, 1, 999, 1, 1, 0)
+    spin = Gtk::SpinButton.new(adjustment, 1, 0)
+    box.pack_start(spin, false, false, 10)
+    frame.add(box)
+    return frame
   end
 end
